d17f4ffbd7501780736cb118f270faff54e057b5
Maria Natália <cardim.natalia@gmail.com>
2025-01-27 15:03:53 -0300

    Construção do display de contatos do admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from contact.models import Contact
 
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    # Display do painel administrativo
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    # Ordem decrescente
+    ordering = '-id',
+    search_fields = 'id', 'first_name', 'last_name'
+    list_per_page = 10
+    list_max_show_all = 50
+    # Torna os itens editáveis sem precisar sair do display
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
