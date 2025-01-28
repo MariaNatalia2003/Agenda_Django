8042f5b8d7d8bc8b68abd9f7e346a6a418ccbcf7
Maria Natália <cardim.natalia@gmail.com>
2025-01-28 14:45:26 -0300

    Implementação do recurso de busca de contatos

--- contact/admin.py
+++ contact/admin.py
@@ -5,14 +5,14 @@ from contact.models import Contact, Category
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     # Display do painel administrativo
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
     # Ordem decrescente
     ordering = '-id',
     search_fields = 'id', 'first_name', 'last_name'
     list_per_page = 10
     list_max_show_all = 50
     # Torna os itens editáveis sem precisar sair do display
-    list_editable = 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name', 'show',
     list_display_links = 'id', 'phone',
 
 @admin.register(Category)
