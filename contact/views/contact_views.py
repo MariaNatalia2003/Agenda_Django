0e77863097ca10334603f50aa7441183e05c68d9
Maria Natália <cardim.natalia@gmail.com>
2025-01-28 15:08:14 -0300

    Paginação da agenda

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -23,10 +27,12 @@ def search(request):
         return redirect('contact:index')
 
     contacts = Contact.objects.filter(show=True) \
-        .filter(Q(first_name__icontains=search_value) |
-        Q(last_name__icontains=search_value) |
-        Q(phone__icontains=search_value) |
-        Q(email__icontains=search_value)) \
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        ) \
         .order_by('-id')
 
     context = {
