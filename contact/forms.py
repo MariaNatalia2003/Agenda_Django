07e1a2a56d286401bacfae6b102a09d904cda0f0
Maria Natália <cardim.natalia@gmail.com>
2025-01-29 10:39:36 -0300

    Página e lógica de update

--- contact/forms.py
+++ contact/forms.py
@@ -21,39 +21,50 @@ class ContactForm(forms.ModelForm):
             #'class': 'classe-a classe-b',
             #'placeholder': 'Aqui veio do init',
         #})
-        
+
     class Meta:
         model = models.Contact
         fields = (
             'first_name',
             'last_name',
+            'phone',
+            'email',
+            'description',
+            'category',
         )
         widgets = {
             'first_name': forms.TextInput(
                 attrs={
-                    'class': 'classe-a classe-b'
+                    'class': 'classe-a classe-b',
                     'placeholder': 'Escreva aqui'
                 }
             )
         }
 
     def clean(self):
-        #cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
+        if first_name == last_name:
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'O primeiro nome não pode ser igual ao segundo nome',
+                    code='invalid'   
+                )
             )
-        )
-
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
+            self.add_error(
+                'last_name',
+                ValidationError(
+                    'O último nome não pode ser igual ao segundo nome',
+                    code='invalid'   
+                )
             )
-        )
 
         return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        return first_name
